3da465d2d8b16aac990331a7fda07b032f947fad
Sanjidul-Alom-Shifat <sanjidul.alom.shifat.official@gmail.com>
2023-05-31 00:06:16 +0600

    final project part 5 server is completed

--- index.js
+++ index.js
@@ -9,6 +9,23 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+        return res.status(401).send({ error: true, message: 'unauthorized access' });
+    }
+    // bearer token
+    const token = authorization.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ error: true, message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
 // mongodb
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -35,8 +52,28 @@ async function run() {
         const cartCollection = client.db("BistroDB").collection("carts");
 
 
+        // post for jwt 
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1h'
+            });
+            res.send({ token });
+        })
+
+        // Warning: use verifyJWT before using verifyAdmin
+        const verifyAdmin = async (req, res, next) => {
+            const email = req.decoded.email;
+            const query = { email: email }
+            const user = await usersCollection.findOne(query);
+            if (user?.role !== 'admin') {
+                return res.status(403).send({ error: true, message: 'forbidden message' });
+            }
+            next();
+        }
+
         // user related apis : for get users
-        app.get('/users', async (req, res) => {
+        app.get('/users', verifyJWT, verifyAdmin, async (req, res) => {
             const result = await usersCollection.find().toArray();
             res.send(result);
         })
@@ -54,6 +91,20 @@ async function run() {
             res.send(result);
         })
 
+        // to ge 
+        app.get('/users/admin/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email;
+
+            if (req.decoded.email !== email) {
+                res.send({ admin: false })
+            }
+            const query = { email: email }
+            const user = await usersCollection.findOne(query);
+            const result = { admin: user?.role === 'admin' }
+            res.send(result);
+        })
+
+
         // for update : to make user admin
         app.patch('/users/admin/:id', async (req, res) => {
             const id = req.params.id;
@@ -64,7 +115,7 @@ async function run() {
                     role: 'admin'
                 },
             };
-            
+
             const result = await usersCollection.updateOne(query, updateUser);
             res.send(result);
         })
@@ -92,11 +143,17 @@ async function run() {
         })
 
         // for get all data
-        app.get('/carts', async (req, res) => {
+        app.get('/carts', verifyJWT, async (req, res) => {
             const email = req.query.email;
             if (!email) {
                 res.send([]);
             }
+
+            const decodedEmail = req.decoded.email;
+            if (email !== decodedEmail) {
+                return res.status(403).send({ error: true, message: 'porviden access' })
+            }
+
             const query = { email: email };
             const result = await cartCollection.find(query).toArray();
             res.send(result);
