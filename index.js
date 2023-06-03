d9889c0ec7e0df56e51643a02c0dd407be7c7d9e
Sanjidul-Alom-Shifat <sanjidul.alom.shifat.official@gmail.com>
2023-06-04 01:26:33 +0600

    vercel added

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
         // Send a ping to confirm a successful connection
 
         const usersCollection = client.db("BistroDB").collection("users");
